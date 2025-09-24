eda311854e23b6dd2ec4b2061496a7635efa93ea
LukeMech <lukaszjajkommz@gmail.com>
2025-09-24 18:41:58 +0200

    meow xD

--- scripts/build-proot.sh
+++ scripts/build-proot.sh
@@ -1,5 +1,6 @@
-git clone https://github.com/proot-me/proot -b 5f780cba57ce7ce557a389e1572e0d30026fcbca
+git clone https://github.com/proot-me/proot
 cd proot
+git checkout 5f780cba57ce7ce557a389e1572e0d30026fcbca
 
 make -C src loader.elf build.h
 make -C src proot care

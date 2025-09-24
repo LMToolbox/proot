3ba793a6d6cad5a86b9f788c455ad1f1edc7fec2
LukeMech <lukaszjajkommz@gmail.com>
2025-09-24 18:39:38 +0200

    Repair arch naming

--- scripts/build-proot.sh
+++ scripts/build-proot.sh
@@ -1,11 +1,10 @@
 git clone https://github.com/proot-me/proot
 cd proot
 
-make -C src loader.elf loader-m32.elf build.h
+make -C src loader.elf build.h
 make -C src proot care
 
 mkdir -p dist
-mv src/proot dist/proot-$arch
-mv src/loader.elf dist/loader-$arch.elf
-mv src/loader-m32.elf dist/loader-m32-$arch.elf
-mv src/care dist/care-$arch
+mv src/proot dist/proot-$ARCH
+mv src/loader.elf dist/loader-$ARCH.elf
+mv src/care dist/care-$ARCH

e988cb337701f5d4c94c126d846004c160b01049
LukeMech <81306360+LukeMech@users.noreply.github.com>
2025-09-25 09:58:50 +0200

    Create set-envs.sh

---
+++ set-envs.sh
@@ -0,0 +1,19 @@
+export TOOLCHAIN=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64
+export TARGET=$ARCH-linux-android
+export API=24
+
+export DWNLD=$(realpath $DWNLD)
+export PREFIX=$(realpath $PREFIX)
+
+export AR=$TOOLCHAIN/bin/llvm-ar
+export CC="$TOOLCHAIN/bin/clang --target=$TARGET$API"
+export AS=$CC
+export CXX="$TOOLCHAIN/bin/clang++ --target=$TARGET$API"
+export LD=$TOOLCHAIN/bin/ld
+export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
+export STRIP=$TOOLCHAIN/bin/llvm-strip
+
+export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH"
+
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"

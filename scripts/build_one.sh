a71025359289c4caaffc1c3145ed6aa24e8c6c75
LukeMech <lukaszjajkommz@gmail.com>
2025-09-25 18:50:29 +0200

    meow

--- scripts/build_one.sh
+++ scripts/build_one.sh
@@ -18,6 +18,7 @@ cd "$PKGDIR"
 
 # Set PREFIX if not set
 if [ -z "$PREFIX" ]; then
+  mkdir -p "$ROOTDIR/dependencies/install"
   export PREFIX="$PKGDIR/install"
 fi
 export PREFIX=$(realpath "$PREFIX")

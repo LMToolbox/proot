076821e4470cf1c79ec83ff026a24b333dcc77f4
LukeMech <81306360+LukeMech@users.noreply.github.com>
2025-09-26 13:08:11 +0200

    Update build_one.sh

--- scripts/build_one.sh
+++ scripts/build_one.sh
@@ -40,9 +40,6 @@ if [ -f pkg.json ]; then
     # shellcheck source=/dev/null
     echo "Auto-configuring build env"
     . "$SETENV"
-  else 
-    echo $SETENV
-    ls
   fi
 
   if [ "$BUILD_TYPE" = "object" ]; then
